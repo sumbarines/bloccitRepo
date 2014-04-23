495f2d726a3c6c7767b53cee2038a70623834cfb
Beverly Vessella <sumbev@gmail.com>
2014-04-23 02:06:38 +0000

    Associated users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,50 @@
 
 require 'faker'
 
-24.times do
-  p = Post.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
-  3.times do
-    p.comments.create(body: Faker::Lorem.paragraph)
-  end
+5.times do
+  user = User.new(
+    name:   Faker::Name.name,
+    email:  Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
+# Note: by calling `User.new` instead of `create`,
+# we create an instance of User which isn't immediately saved to the database.
+
+# The `skip_confirmation!` method sets the `confirmed_at` attribute
+# to avoid triggering an confirmation email when the User is saved.
+
+# The `save` method then saves this User to the database.
+
+# Create Posts
+50.times do
+  Post.create(
+    user: users.sample,
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+  )
+end
+posts = Post.all
+
+#Create Comments
+100.times do
+  Comment.create(
+    #user: users.sampe, # we have no yet associate users with comments
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+    )
 end
 
+User.first.update_attributes(
+  email: 'sumbev@gmail.com',
+  password: 'helloworld',
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
