748533086d3087bdb7e6ef19d112ac646442ffce
Beverly Vessella <sumbev@gmail.com>
2014-04-27 20:02:35 +0000

    Cleaned up registration views and app layout.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
     @post.topic = @topic
     #raise
     authorize @post
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else
@@ -47,4 +47,11 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
