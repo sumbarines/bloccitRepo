aadb2ad2459ad2f71b98b71807d8fab47594083f
Beverly Vessella <sumbev@gmail.com>
2014-04-29 05:41:49 +0000

    Added image to Post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,7 +51,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :image)
   end
 
 end
