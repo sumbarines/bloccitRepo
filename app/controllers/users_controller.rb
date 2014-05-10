ada21506c966b518443f32e8255fb49d4fcb4ab1
Beverly Vessella <sumbev@gmail.com>
2014-05-10 03:45:08 +0000

    Interlude changes for registration, email permissions, and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
 
   def user_params
     #params.require(:user).permit(:name, :avatar)
-    params.require(:user).permit(:name, :avatar, :avatar_cache)
+    params.require(:user).permit(:name, :avatar, :email_favorites)
   end
 end
